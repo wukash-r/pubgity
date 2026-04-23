6fe460e273af63d5c3235dc49455f7d7eba7515b
wukash-r <lukasz.m.roszkiewicz@gmail.com>
2026-04-24 01:10:28 +0200

    Init

---
+++ src/main/kotlin/org/traanite/pubgity/PubgityApplication.kt
@@ -0,0 +1,11 @@
+package org.traanite.pubgity
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PubgityApplication
+
+fun main(args: Array<String>) {
+    runApplication<PubgityApplication>(*args)
+}
